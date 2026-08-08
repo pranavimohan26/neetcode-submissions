9f4ab0157f9d13b7eb6a51bf8f0fc0441f7bd37f
Pranavi Mohan <pranavimohan26@gmail.com>
2026-08-08 21:44:29 +0530

    Add: sort-array-by-parity - submission-0

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int index = 0;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i]%2 == 0){
+            result[index] = nums[i];
+            index++;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(nums[i]%2 != 0){
+            result[index] = nums[i];
+            index++;
+            }
+        }
+
+        return result;
+    }
+}
